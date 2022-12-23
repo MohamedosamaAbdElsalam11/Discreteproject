beb021c1e31ee4016846709192838318a7dad682
MohamedosamaAbdElsalam11 <121316773+MohamedosamaAbdElsalam11@users.noreply.github.com>
2022-12-23 23:17:59 +0200

    Add files via upload

---
+++ The perfect numbers project.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Program2
+{
+    public class Welcome 
+    {         
+        public static void Main()
+		{
+			
+				//mohammed osama abd El_salam
+			
+			Console.WriteLine("Enter the first number");
+			int num1 =int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the last number");
+			int num2 =int.Parse(Console.ReadLine());
+			
+			Console.WriteLine("The list of perfect numbers between the number " + "\'"+(num1)+"\'" + " and the number " + "\'" +(num2)+ "\'" + " is : ");
+			
+			
+			int sum=0;
+			for(int i=num1; i<=num2; ++i)
+			{
+				for(int x=num1; x<=num2; ++x)
+				{
+					if(x<i)
+					{
+						if(i%x==0)
+						sum +=x;
+					}
+				}
+				if(sum==i)
+				{
+					Console.WriteLine(i);
+				}
+				
+				sum=0; 
+			}
+			return;
+		}
+    }
+}
