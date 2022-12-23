9855db577eda23e0a5b46ace75d0ac704a518c9c
MohamedosamaAbdElsalam11 <121316773+MohamedosamaAbdElsalam11@users.noreply.github.com>
2022-12-23 22:47:38 +0200

    Add files via upload

---
+++ The prime numbers project.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+			//mohammed osama abd El_salam
+			
+			Console.WriteLine("Enter the first number");
+			int num1 =int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the last number");
+			int num2 =int.Parse(Console.ReadLine());
+			
+			
+			Console.WriteLine("The list of prime numbers between the number " + "\'"+(num1)+"\'" + " and the number " + "\'" +(num2)+ "\'" + " is : ");
+			
+			int val =0;
+			for(int i=num1; i<=num2; ++i)
+			{
+				for(int z=1; z<=i; ++z)
+				{
+					if(i%z==0)
+					val++;
+				}
+				if(val==2)
+				{
+					Console.WriteLine(i);
+					val=0;
+				}
+				else
+				{
+					val=0;
+					continue;
+				}
+			}
+					
+			
+			
+		}
+	}
+}
